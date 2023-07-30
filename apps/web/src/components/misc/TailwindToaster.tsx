685b4d4bca0905c8c1c07496e07d21518fcc9eb6
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2023-07-30 18:01:07 +0200

    Improvements on frontend

--- apps/web/src/components/misc/TailwindToaster.tsx
+++ apps/web/src/components/misc/TailwindToaster.tsx
@@ -7,17 +7,19 @@ export default function TailwindToaster() {
   return (
     <Toaster position="top-right">
       {(t) => (
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           {t.visible && (
             <motion.div
               transition={{ duration: 0.15 }}
               initial={{ scale: 0.5, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.75, opacity: 0 }}
-              className="transform p-4 flex bg-white dark:bg-gray-900 rounded shadow-lg"
+              className="flex max-w-sm p-4 origin-top-right transform bg-white rounded shadow-lg dark:bg-gray-900"
             >
-              <ToastIcon toast={t} />
-              <p className="px-2">{resolveValue(t.message, t)}</p>
+              <div className="w-4 h-4">
+                <ToastIcon toast={t} />
+              </div>
+              <p className="px-2 ml-2 text-sm">{resolveValue(t.message, t)}</p>
             </motion.div>
           )}
         </AnimatePresence>
--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
+import { toast } from 'react-hot-toast';
 import { useQuery } from 'react-query';
+
+import { AxiosError } from 'axios';
 
 import { getRandomMessage } from '@/api/test';
 import Button from '@/components/utils/Button';
+import Spinner from '@/components/utils/Spinner';
 
 export default function HomePage() {
-  const { data, isLoading, isFetching, refetch } = useQuery('random-message', getRandomMessage);
+  const { data, isLoading, isFetching, refetch } = useQuery('random-message', getRandomMessage, {
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toast.error('Request Failed, please check your api server');
+      }
+    },
+  });
 
   return (
     <main className="grid h-full place-items-center">
@@ -12,9 +22,11 @@ export default function HomePage() {
         <h1 className="mb-4 text-xl font-bold text-gray-950 dark:text-white">
           NestJS + ReactJS starter
         </h1>
-        <p className="px-2 py-1 mx-auto text-sm font-medium text-gray-600 bg-gray-100 border border-gray-200 rounded-md w-fit dark:text-gray-400 dark:bg-gray-900 dark:border-gray-800">
-          {data}
-        </p>
+        {data && (
+          <p className="px-2 py-1 mx-auto text-sm font-medium text-gray-600 bg-gray-100 border border-gray-200 rounded-md w-fit dark:text-gray-400 dark:bg-gray-900 dark:border-gray-800">
+            {data}
+          </p>
+        )}
         <Button
           disabled={isLoading || isFetching}
           onClick={() => refetch()}
@@ -22,6 +34,7 @@ export default function HomePage() {
           className="mx-auto mt-4"
           size="sm"
         >
+          {(isLoading || isFetching) && <Spinner className="mr-2" />}
           Randomize Text
         </Button>
       </div>
